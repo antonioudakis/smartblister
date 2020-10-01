848d92e09da4c9dfa8e6d5f14820fde1a4c86216
antonioudakis <t.antonioudakis@gmail.com>
2020-10-01 14:07:32 +0300

    Moved left navbar-brand

--- smartblister/settings.py
+++ smartblister/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.1.142','147.102.213.13']
 
 
 # Application definition
